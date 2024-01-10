436f75047e31aa4cb46588d8304192711e34325d
Abigaeldis <abigael.disegna@gmail.com>
2024-01-10 09:37:33 +0100

    Init project

--- src/bo/Menu.java
+++
@@ -1,9 +0,0 @@
-package bo;
-
-public class Menu {
-	
-	 public static void afficherMenu() {
-	        System.out.println("Entrez votre choix :");
-	        System.out.println("1. \n"+"2. \n");
-	    }
-}
