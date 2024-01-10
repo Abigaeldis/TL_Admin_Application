dba3b00d75c9d98667105454f9262c680f3c1a93
Abigaeldis <abigael.disegna@gmail.com>
2024-01-10 16:57:47 +0100

    appliacation console cree restaurant

--- src/controller/ApplicationConsole.java
+++ src/controller/ApplicationConsole.java
@@ -5,6 +5,7 @@ import bll.BLLException;
 import bll.PlatBLL;
 import bll.RestaurantBLL;
 import bll.TableBLL;
+import bo.Restaurant;
 
 public class ApplicationConsole {
 	private static Scanner scan;
@@ -19,7 +20,7 @@ public class ApplicationConsole {
 			tableBll = new TableBLL();
 			restaurantBll = new RestaurantBLL();
 			platBll = new PlatBLL();
-			
+
 		} catch (BLLException e) {
 			e.printStackTrace();
 		}
@@ -32,6 +33,18 @@ public class ApplicationConsole {
 
 			switch (choix) {
 			case 1: 
+				System.out.println("Veuillez choisir l'action à réaliser");
+				System.out.println("1.Saisie de restaurant manuelle");
+				System.out.println("2.Saisie de restaurant automatique");
+				int decision = scan.nextInt();
+				scan.nextLine();
+				if (decision == 1) {
+					creerRestaurantManuel();
+				}
+				else if (decision == 2) {
+					creerRestaurantAuto();
+				}
+				
 				//Ajouter un restaurant
 				break;
 			case 2:
@@ -72,4 +85,31 @@ public class ApplicationConsole {
 		return choix;
 	}
 
+	private static void creerRestaurantManuel() {
+		System.out.println("Vous avez choisi d'ajouter un restaurant");
+
+		System.out.println("Veuillez saisir le nom du restaurant");
+		String nom = scan.nextLine();
+
+		System.out.println("Veuillez saisir l'adresse du restaurant");
+		String adresse = scan.nextLine();
+
+		System.out.println("Veuillez saisir une description pour votre restaurant");
+		String description = scan.nextLine();
+
+		try {
+			Restaurant restaurantAjoute = restaurantBll.insert(nom, adresse, description);
+			System.out.println("Restaurant ajouté avec succès " + restaurantAjoute);
+		} catch (BLLException e) {
+			System.out.println("Une erreur est survenue :");
+			for (String erreur : e.getErreurs()) {
+				System.out.println("\t" + erreur);
+			}
+			e.printStackTrace();
+		}
+	}
+	
+	private static void creerRestaurantAuto() {
+		
+	}
 }
